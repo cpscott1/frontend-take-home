551b6303c95aa87c5ad84c393cc58b91964fed0b
cpscott1 <thacam18gmail.com>
2021-03-12 13:16:42 -0600

    able to fetch data

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,19 @@ import { Link } from 'react-router-dom'
 import { Box, Grid, Text, Image } from '@chakra-ui/react'
 
 const Home = () => {
+
+  const [shop, setShop] = useState({
+    products: [],
+    search: ''
+  })
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then(res=>res.json())
+      .then(products => setShop({ ...shop, products: products }))
+      console.log(shop)
+  }, [])
+
   return (
     <Box>
       <Text>Home</Text>
