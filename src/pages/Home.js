331be8e423f5d066075bf6c6ceea1514216422d0
cpscott1 <thacam18gmail.com>
2021-03-12 12:59:29 -0600

    added home and product page

---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { Box, Grid, Text, Image } from '@chakra-ui/react'
+
+const Home = () => {
+  return (
+    <Box>
+      <Text>Home</Text>
+    </Box>
+  )
+}
+
+export default Home
---
+++ src/pages/Product.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+import { Box, Grid, Text, Image } from '@chakra-ui/react'
+
+const Product = () => {
+  return (
+    <Box>
+      <Text>Home</Text>
+    </Box>
+  )
+}
+
+export default Product
